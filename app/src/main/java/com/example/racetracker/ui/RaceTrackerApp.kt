8196cbec5e33721861b749f013b1c1240f5fb1da
Samrat Parajuli <code.samzt13@gmail.com>
2026-02-10 16:47:36 +0545

    Init

--- app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -1,8 +1,79 @@
 package com.example.racetracker.ui
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.racetracker.R
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 @Composable
 fun RaceTrackerApp(){
+    val playerOne = remember{
+        RaceParticipant("Player 1", progressIncrement = 1)
+    }
 
+    val playerTwo = remember {
+        RaceParticipant("Player 2", progressIncrement = 2)
+    }
+
+    var raceInProgress by remember { mutableStateOf(false) }
+
+    if(raceInProgress){
+        LaunchedEffect(playerOne, playerTwo) {
+            coroutineScope {
+                launch { playerOne.run() }
+                launch { playerTwo.run() }
+            }
+            raceInProgress = false
+        }
+    }
+
+    RaceTrackerScreen(
+        playerOne = playerOne,
+        playerTwo = playerTwo,
+        isRunning = raceInProgress,
+        onRunStateChange = { raceInProgress = it },
+        modifier = Modifier
+            .statusBarsPadding()
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding()
+            .padding(horizontal = 16.dp),
+    )
+}
+
+@Composable
+private fun RaceTrackerScreen(
+    playerOne: RaceParticipant,
+    playerTwo: RaceParticipant,
+    isRunning: Boolean,
+    onRunStateChange: (Boolean) -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = stringResource(R.string.run_a_race)
+        )
+    }
 }
