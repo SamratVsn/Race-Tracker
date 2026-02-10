ffeec8c7bcd3609dcde5c44d0e541ff6f5ab39bb
Samrat Parajuli <code.samzt13@gmail.com>
2026-02-11 05:29:49 +0545

    UI

--- app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
+++ app/src/main/java/com/example/racetracker/ui/RaceTrackerApp.kt
@@ -2,12 +2,23 @@ package com.example.racetracker.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -17,9 +28,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.racetracker.R
+import com.example.racetracker.ui.theme.RaceTrackerTheme
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
@@ -73,7 +89,130 @@ private fun RaceTrackerScreen(
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         Text(
-            text = stringResource(R.string.run_a_race)
+            text = stringResource(R.string.run_a_race),
+            style = MaterialTheme.typography.headlineSmall
         )
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ){
+            Icon(
+                painter = painterResource(R.drawable.ic_walk),
+                contentDescription = null
+            )
+            StatusIndicator(
+                participantName = playerOne.name,
+                currentProgress = playerOne.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerOne.maxProgress
+                ),
+                progressFactor = playerOne.progressFactor,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.size(24.dp))
+            StatusIndicator(
+                participantName = playerTwo.name,
+                currentProgress = playerTwo.currentProgress,
+                maxProgress = stringResource(
+                    R.string.progress_percentage,
+                    playerTwo.maxProgress
+                ),
+                progressFactor = playerTwo.progressFactor,
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Spacer(modifier = Modifier.size(24.dp))
+            RaceControls(
+                isRunning = isRunning,
+                onRunStateChange = onRunStateChange,
+                onReset = {
+                    playerOne.reset()
+                    playerTwo.reset()
+                    onRunStateChange(false)
+                },
+                modifier = Modifier.fillMaxWidth(),
+            )
+        }
+    }
+}
+
+@Composable
+private fun StatusIndicator(
+    participantName: String,
+    currentProgress: Int,
+    maxProgress: String,
+    progressFactor: Float,
+    modifier: Modifier = Modifier
+){
+    Row(
+        modifier = modifier
+    ) {
+        Text(
+            text = participantName,
+            modifier = Modifier.padding(end = 8.dp)
+        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            LinearProgressIndicator(
+                progress = progressFactor,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(16.dp)
+                    .clip(RoundedCornerShape(4.dp))
+            )
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = stringResource(R.string.progress_percentage, currentProgress),
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.weight(1f)
+                )
+                Text(
+                    text = maxProgress,
+                    textAlign = TextAlign.End,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun RaceControls(
+    onRunStateChange: (Boolean) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier,
+    isRunning: Boolean = true,
+){
+    Column(
+        modifier = modifier.padding(top = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Button(
+            onClick = { onRunStateChange(!isRunning) },
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(if (isRunning) stringResource(R.string.pause) else stringResource(R.string.start))
+        }
+        OutlinedButton(
+            onClick = onReset,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(stringResource(R.string.reset))
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun RacePreview(){
+    RaceTrackerTheme {
+        RaceTrackerApp()
     }
 }
